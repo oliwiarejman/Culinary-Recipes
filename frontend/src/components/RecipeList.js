45df93b290c7de53d9a8b8b09bbd57efd1420773
Oliwia Rejman <o.rejman.418@studms.ug.edu.pl>
2024-01-28 22:53:59 +0100

    :bulb: Dodanie wyszukiwania po nazwie

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -1,14 +1,18 @@
+// W RecipeList.js
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 const RecipeList = () => {
   const [recipes, setRecipes] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
 
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/recipes");
+        const response = await axios.get(
+          `http://localhost:3000/api/recipes/search?title=${searchTitle}`
+        );
         setRecipes(response.data);
       } catch (error) {
         console.error("Błąd pobierania przepisów:", error.message);
@@ -16,13 +20,45 @@ const RecipeList = () => {
     };
 
     fetchRecipes();
-  }, []);
+  }, [searchTitle]);
+
+  const handleSearchChange = (e) => {
+    setSearchTitle(e.target.value);
+  };
 
   return (
     <div style={{ maxWidth: "600px", margin: "auto" }}>
       <h2 style={{ fontSize: "24px", marginBottom: "16px", textAlign: "center" }}>
         Lista Przepisów
       </h2>
+      <div style={{ marginBottom: "16px", textAlign: "center" }}>
+        <input
+          type="text"
+          placeholder="Szukaj po tytule"
+          value={searchTitle}
+          onChange={handleSearchChange}
+          style={{
+            marginBottom: ".5rem",
+            padding: "8px",
+            width: "80%",
+            marginRight: "8px",
+            borderRadius: "5px",
+          }}
+        />
+        <button
+          onClick={() => setSearchTitle("")}
+          style={{
+            padding: "8px",
+            background: "#4CAF50",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Wyczyść
+        </button>
+      </div>
       <ul style={{ listStyle: "none", padding: 0 }}>
         {recipes.map((recipe) => (
           <li
